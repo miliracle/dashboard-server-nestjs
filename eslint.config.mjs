6c1829e2b336dd687fdf4ed1b40591bdf6f71ee9
miliracle <nguyennhathan126@gmail.com>
2024-12-18 17:06:53 +0700

    chore: add project config

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/build/**'],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+];
