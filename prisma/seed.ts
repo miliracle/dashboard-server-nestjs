461ab580b1d4b092fddad1495c863a0dc3662672
miliracle <nguyennhathan126@gmail.com>
2024-12-18 18:33:44 +0700

    chore: integrate with prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,65 @@
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+const db = new PrismaClient();
+
+const main = async () => {
+  const orderData = [...Array(10)].map(() => ({
+    id: faker.number.int({ min: 10, max: 170 }),
+    total: faker.number.float({ min: 7, max: 15657, fractionDigits: 2 }),
+    createdAt: faker.date.anytime(),
+    updatedAt: faker.date.anytime(),
+    userId: 5,
+    stripeInvoiceId: faker.string.alphanumeric(15),
+  }));
+
+  const productData = [...Array(10)].map(() => ({
+    id: faker.lorem.word(),
+    name: faker.commerce.productName(),
+    price: faker.number.float({ min: 35, max: 1055, fractionDigits: 2 }),
+    createdAt: faker.date.anytime(),
+    updatedAt: faker.date.anytime(),
+  }));
+
+  await db.user.create({
+    data: {
+      id: faker.number.int({ min: 5, max: 10 }),
+      name: faker.person.fullName(),
+      address: faker.location.streetAddress(),
+      email: faker.internet.email(),
+      roles: ['Customer', 'Support', 'Store'],
+    },
+  });
+
+  productData.map(async (product) => {
+    await db.product.create({
+      data: {
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        createdAt: product.createdAt,
+        updatedAt: product.updatedAt,
+      },
+    });
+  });
+
+  orderData.map(async (order) => {
+    await db.order.create({
+      data: {
+        id: order.id,
+        total: order.total,
+        createdAt: order.createdAt,
+        updatedAt: order.updatedAt,
+        userId: order.userId,
+      },
+    });
+  });
+};
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await db.$disconnect();
+  });
